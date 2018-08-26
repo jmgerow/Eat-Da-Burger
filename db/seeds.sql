34aeefb5b139a702177a0fd6850292a763479764
jmgerow <justingerow1@gmail.com>
2018-08-26 13:50:18 -0500

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom & Swiss Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon, Egg, and Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', false);
